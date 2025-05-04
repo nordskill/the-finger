d7384ad1e0d8e0dad4bb218e622b10dbbd4b037a
Oleksii <26419775+nordskill@users.noreply.github.com>
2025-05-05 00:56:35 +0200

    fix: update gesture type display and improve HTML structure

--- js/test.js
+++ js/test.js
@@ -8,12 +8,12 @@ const finger = new TheFinger(touchArea, {
 
 const gestureType = document.querySelector('#gesture_type');
 
-finger.track('tap', gesture => { gestureType.innerText = 'tap'; });
-finger.track('double-tap', gesture => { gestureType.innerText = 'double-tap'; });
-finger.track('press', gesture => { gestureType.innerText = 'press'; });
-finger.track('long-press', gesture => { gestureType.innerText = 'long-press'; });
-// finger.track('rotate', gesture => { gestureType.innerText = 'rotate'; });
+// finger.track('tap', gesture => { gestureType.innerText = 'tap'; });
+// finger.track('double-tap', gesture => { gestureType.innerText = 'double-tap'; });
+// finger.track('press', gesture => { gestureType.innerText = 'press'; });
+// finger.track('long-press', gesture => { gestureType.innerText = 'long-press'; });
+// finger.track('rotate', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
 // finger.track('pinch-spread', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
-finger.track('drag', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
+// finger.track('drag', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
 finger.track('pan', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
 // finger.track('two-finger-tap', gesture => { gestureType.innerText = 'two-finger-tap'; });
