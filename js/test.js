3759d34ca5c838bf530ec6b277ada83ab2745b59
Oleksii <26419775+nordskill@users.noreply.github.com>
2025-05-04 20:02:42 +0200

    fix: update gesture type display and clean up unused gesture tracking

--- js/test.js
+++ js/test.js
@@ -12,11 +12,8 @@ finger.track('tap', gesture => { gestureType.innerText = 'tap'; });
 finger.track('double-tap', gesture => { gestureType.innerText = 'double-tap'; });
 finger.track('press', gesture => { gestureType.innerText = 'press'; });
 finger.track('long-press', gesture => { gestureType.innerText = 'long-press'; });
-finger.track('rotate', gesture => { gestureType.innerText = 'rotate'; });
-finger.track('pinch-spread', gesture => { gestureType.innerText = 'pinch-spread'; });
-finger.track('drag', gesture => {
-    gesture.flick ? gestureType.innerText = 'flick' : gestureType.innerText = 'drag';
-    console.log(gesture);
-});
-// finger.track('pan', gesture => { gestureType.innerText = 'pan'; });
+// finger.track('rotate', gesture => { gestureType.innerText = 'rotate'; });
+// finger.track('pinch-spread', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
+finger.track('drag', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
+finger.track('pan', gesture => { gestureType.innerText = JSON.stringify(gesture, null, 2) });
 // finger.track('two-finger-tap', gesture => { gestureType.innerText = 'two-finger-tap'; });
