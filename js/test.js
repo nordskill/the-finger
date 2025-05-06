3eadc45b7f5d05918653e7d100bc0b284959dadb
Oleksii <26419775+nordskill@users.noreply.github.com>
2025-05-07 00:57:01 +0200

    refactor: comment out all gesture tracking by default

--- js/test.js
+++ js/test.js
@@ -9,18 +9,24 @@ const finger = new TheFinger(touchArea, {
 const gestureType = document.querySelector('#gesture_type');
 const gestureType2 = document.querySelector('#gesture_type_2');
 
-finger.track('tap', showGestureDetails);
-finger.track('double-tap', showGestureDetails);
-finger.track('press', showGestureDetails);
-finger.track('long-press', showGestureDetails);
-finger.track('rotate', showGestureDetails);
-finger.track('pinch-spread', showGestureDetails);
-finger.track('drag', gesture => showGestureDetails);
-finger.track('pan', gesture => showGestureDetails);
-finger.track('two-finger-tap', showGestureDetails);
+// finger.track('tap', showGestureDetails);
+// finger.track('double-tap', showGestureDetails);
+// finger.track('press', showGestureDetails);
+// finger.track('long-press', showGestureDetails);
+// finger.track('rotate', showGestureDetails);
+// finger.track('pinch-spread', showGestureDetails);
+// finger.track('drag', showGestureDetails);
+// finger.track('pan', showGestureDetails);
+// finger.track('two-finger-tap', showGestureDetails);
 
 function showGestureDetails(gesture) {
     gestureType.innerText = JSON.stringify(gesture, null, 2);
+    // triangle.style.transform = `rotate(${gesture.angle}deg)`;
+    // triangle.style.left = `${gesture.x}px`;
+    // triangle.style.top = `${gesture.y}px`;
+}
+function showGestureDetails2(gesture) {
+    gestureType2.innerText = JSON.stringify(gesture, null, 2);
     // triangle.style.transform = `rotate(${gesture.angle}deg)`;
     // triangle.style.left = `${gesture.x}px`;
     // triangle.style.top = `${gesture.y}px`;
